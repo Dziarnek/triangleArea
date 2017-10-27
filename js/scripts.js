a3479eb645d4ac3e48c4827d91d4b07ea67f7fe3
Dziarnek <kuba.dziarnowski@onet.eu>
2017-10-27 04:50:01 +0200

    Creating project

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+alert("Witaj, na tej stronie możesz obliczyć pole trójkąta!");
+console.log("Witaj, na tej stronie możesz obliczyć pole trójkąta!");
+
+var a = 1,
+    h = 1,
+    triangleArea = 1;
+
+a = prompt("Podaj długość podstawy trójkąta");
+h = prompt("Podaj wysokość trójkąta");
+triangleArea = a*h/2
+
+alert("Pole trójkąta o podstawie " + a + " i wysokości " + h + " wynosi " + triangleArea)
+console.log("Pole trójkąta o podstawie " + a + " i wysokości " + h + " wynosi " + triangleArea)
