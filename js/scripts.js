bca6f9af533ac2f9d9272e3f7cff6224229de46f
Dziarnek <32908239+Dziarnek@users.noreply.github.com>
2017-10-27 06:01:03 +0200

    Bugfix

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ var a = 1,
 
 a = prompt("Podaj długość podstawy trójkąta");
 h = prompt("Podaj wysokość trójkąta");
-triangleArea = a*h/2
+triangleArea = a*h/2;
 
-alert("Pole trójkąta o podstawie " + a + " i wysokości " + h + " wynosi " + triangleArea)
-console.log("Pole trójkąta o podstawie " + a + " i wysokości " + h + " wynosi " + triangleArea)
+alert("Pole trójkąta o podstawie " + a + " i wysokości " + h + " wynosi " + triangleArea);
+console.log("Pole trójkąta o podstawie " + a + " i wysokości " + h + " wynosi " + triangleArea);
